f33120ffcc8d942fbc3b1adb7e7e144ceb91f588
Adrian Sutherland <adrian@sutherlandonline.org>
2020-12-16 20:48:00 +0000

    F0037

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* Modified for VM/370 CMS and GCC by Robert O'Hara, July 2010. */
 
 /* The one version to rule them all! */
-#define CMS_VERSION "0.9.10"
+#define CMS_VERSION "F0037"
 /*
 #define CMS_VERSION "0.9.8"
 #define CMS_VERSION "F0020"
