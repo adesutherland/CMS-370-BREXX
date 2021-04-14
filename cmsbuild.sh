c3c2406a66bd7a849799f2b251529d058d8214ff
Adrian Sutherland <adrian@sutherlandonline.org>
2021-04-14 13:19:08 +0100

    v1.0.0 / f0049 (#70)
    * Initial Test for f0049
    
    * Fix 1
    
    * Fix 3

--- cmsbuild.sh
+++ cmsbuild.sh
@@ -4,101 +4,168 @@
 # Exit if there is an error
 set -e
 
+# Get the latest gccbrx.cckd disk image
+herccontrol "detach 09F0"
+wget -nv https://github.com/adesutherland/CMS-370-GCCLIB/releases/download/v1.0.0/GCCLIB.zip
+unzip GCCLIB.zip
+cp GCCLIB/gccbrx.cckd ..
+rm GCCLIB.zip
+rm -r GCCLIB
+herccontrol "attach 09F0 3350 gccbrx.cckd"
+
 # IPL
-herccontrol -v
 herccontrol "ipl 141" -w "USER DSC LOGOFF AS AUTOLOG1"
 herccontrol "/cp start c" -w "RDR"
 herccontrol "/cp start d class a" -w "PUN"
 
-# LOGON CMSUSER
+# LOGON MAINTC
 herccontrol "/cp disc" -w "^VM/370 Online"
-herccontrol "/logon cmsuser cmsuser" -w "^CMS VERSION"
+herccontrol "/logon maintc maintc" -w "^CMS"
 herccontrol "/" -w "^Ready;"
+herccontrol "/purge rdr" -w "^Ready;"
+
+herccontrol "/ACCESS 393 B" -w "^Ready;"
+herccontrol "/ERASE * * B1" -w "^Ready;"
 
 # Get Source
-yata -c
+yata -c -f archive.yata
 herccontrol -m >tmp; read mark <tmp; rm tmp
-echo "USERID  CMSUSER\n:READ  YATA     TXT     " > tmp
-cat yata.txt >> tmp
+echo "USERID MAINTC\n:READ  ARCHIVE  YATA    " > tmp
+cat archive.yata >> tmp
 netcat -q 0 localhost 3505 < tmp
 rm tmp
 herccontrol -w "HHCRD012I" -f $mark
 herccontrol "/" -w "RDR FILE"
-herccontrol "/yata -x -f READER -d f" -w "^Ready;"
+herccontrol "/yata -x -f READER -d b" -w "^Ready;"
 
 # Get test suite
-yata -c -d tests
+yata -c -d tests -f archive.yata
 herccontrol -m >tmp; read mark <tmp; rm tmp
-echo "USERID  CMSUSER\n:READ  YATA     TXT     " > tmp
-cat yata.txt >> tmp
+echo "USERID  MAINTC\n:READ  ARCHIVE  YATA    " > tmp
+cat archive.yata >> tmp
 netcat -q 0 localhost 3505 < tmp
 rm tmp
 herccontrol -w "HHCRD012I" -f $mark
 herccontrol "/" -w "RDR FILE"
-herccontrol "/read *" -w "^Ready;"
-herccontrol "/yata -x -d f" -w "^Ready;"
-herccontrol "/erase yata txt a" -w "^Ready;"
+herccontrol "/yata -x -f READER -d b" -w "^Ready;"
+
+# Get test tools
+yata -c -d tools -f archive.yata
+herccontrol -m >tmp; read mark <tmp; rm tmp
+echo "USERID  MAINTC\n:READ  ARCHIVE  YATA    " > tmp
+cat archive.yata >> tmp
+netcat -q 0 localhost 3505 < tmp
+rm tmp
+herccontrol -w "HHCRD012I" -f $mark
+herccontrol "/" -w "RDR FILE"
+herccontrol "/yata -x -f READER -d b" -w "^Ready;"
+
+# Fix Source Files
+herccontrol "/COPYFILE * MACRO    B (RECFM F LRECL 80" -w "^Ready"
+herccontrol "/COPYFILE * COPY     B (RECFM F LRECL 80" -w "^Ready"
+herccontrol "/COPYFILE * ASSEMBLE B (RECFM F LRECL 80" -w "^Ready"
+#herccontrol "/COPYFILE * EXEC     B (RECFM F" -w "^Ready"
+herccontrol "/RENAME NEWBREXX TMPFTYPE B NEWBREXX CONTROL B" -w "^Ready"
 
 # Make source tape and vmarc
 herccontrol "/cp disc" -w "^VM/370 Online"
 herccontrol "/logon operator operator" -w "RECONNECTED AT"
 hetinit -n -d brexxsrc.aws
 herccontrol "devinit 480 io/brexxsrc.aws" -w "^HHCPN098I"
-herccontrol "/attach 480 to cmsuser as 181" -w "TAPE 480 ATTACH TO CMSUSER"
+herccontrol "/attach 480 to maintc as 181" -w "TAPE 480 ATTACH"
 herccontrol "devinit 00d io/brexxsrc.vmarc" -w "^HHCPN098I"
 herccontrol "/cp disc" -w "^VM/370 Online"
-herccontrol "/logon cmsuser cmsuser" -w "RECONNECTED AT"
+herccontrol "/logon maintc maintc" -w "RECONNECTED"
 herccontrol "/begin"
-herccontrol "/COPYFILE * ASSEMBLE &DISK (RECFM F LRECL 80"
-herccontrol "/tape dump * * f (noprint" -w "^Ready;"
+herccontrol "/tape dump * * b (noprint" -w "^Ready;"
 herccontrol "/detach 181" -w "^Ready;"
-herccontrol "/vmarc pack * * f (pun notrace" -w "^Ready;"
+herccontrol "/vmarc pack * * b (pun notrace" -w "^Ready;"
 
-# Build
-herccontrol "/ACC 193 E (ERASE" -w "^Ready;"
-herccontrol "/mkbrexx DEBUG" -w "^Ready;" -t 350
-herccontrol "/copy brexx text e brexxd = =" -w "^Ready;"
-herccontrol "/copy brexx module e brexxd = =" -w "^Ready;"
-herccontrol "/mkbrexx" -w "^Ready;" -t 350
-herccontrol "/rename * * e = = e2" -w "^Ready;"
+# Close and remove extra record from VMARC file
+herccontrol "devinit 00d dummy" -w "^HHCPN098I"
+truncate -s-80 brexxsrc.vmarc
 
-# Sanity test
-herccontrol "/copy * * e = = a ( replace" -w "^Ready;"
-herccontrol "/copy sysprof exec s = = a" -w "^Ready;"
-herccontrol "/ipl cms" -w "^CMS VERSION"
+# Put tools in the T drive
+herccontrol "/COPYFILE BRXASM EXEC B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE BRXASM EXEC B" -w "^Ready"
+
+herccontrol "/COPYFILE BRXBUILD EXEC B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE BRXBUILD EXEC B" -w "^Ready"
+
+herccontrol "/COPYFILE BRXCOMP EXEC B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE BRXCOMP EXEC B" -w "^Ready"
+
+herccontrol "/COPYFILE BRXCOMPD EXEC B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE BRXCOMPD EXEC B" -w "^Ready"
+
+herccontrol "/COPYFILE BRXGEN EXEC B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE BRXGEN EXEC B" -w "^Ready"
+
+herccontrol "/COPYFILE BRXSRCH EXEC B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE BRXSRCH EXEC B" -w "^Ready"
+
+herccontrol "/COPYFILE BRXSRCHD EXEC B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE BRXSRCHD EXEC B" -w "^Ready"
+
+herccontrol "/COPYFILE BRXSRCHT EXEC B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE BRXSRCHT EXEC B" -w "^Ready"
+
+herccontrol "/COPYFILE TOOLDISK MEMO B = = T (REPLACE" -w "^Ready"
+herccontrol "/ERASE TOOLDISK MEMO B" -w "^Ready"
+
+herccontrol "/COPYFILE NEWBREXX CONTROL B = = A (REPLACE" -w "^Ready"
+herccontrol "/ERASE NEWBREXX CONTROL B" -w "^Ready"
+
+herccontrol "/ipl cms" -w "^CMS"
 herccontrol "/" -w "^Ready;"
-herccontrol "/DMSREX" -w "^Ready;"
-herccontrol "/rexxtry" -w "^Rexxtry;"
-herccontrol "/parse version myver" -w "^Rexxtry;"
-herccontrol "/say myver" -w "^Rexxtry;"
-herccontrol "/exit" -w "^Ready;"
 
-# Test suite
-herccontrol "/runtest_" -w "^Ready;"
+herccontrol "/BRXBUILD" -w "^Ready;" -t 240
+herccontrol "/ipl cms" -w "^CMS"
+herccontrol "/" -w "^Ready;"
+
+herccontrol "/BRXSRCH" -w "^Ready;"
+herccontrol "/BRXGEN" -w "^Ready;"
+herccontrol "/ipl cms" -w "^CMS"
+herccontrol "/" -w "^Ready;"
 
 # Make binary tape and vmarc
 herccontrol "/cp disc" -w "^VM/370 Online"
 herccontrol "/logon operator operator" -w "RECONNECTED AT"
 hetinit -n -d brexxbin.aws
 herccontrol "devinit 480 io/brexxbin.aws" -w "^HHCPN098I"
-herccontrol "/attach 480 to cmsuser as 181" -w "TAPE 480 ATTACH TO CMSUSER"
+herccontrol "/attach 480 to maintc as 181" -w "TAPE 480 ATTACH"
 herccontrol "devinit 00d io/brexxbin.vmarc" -w "^HHCPN098I"
 herccontrol "/cp disc" -w "^VM/370 Online"
-herccontrol "/logon cmsuser cmsuser" -w "RECONNECTED AT"
+herccontrol "/logon maintc maintc" -w "RECONNECTED AT"
 herccontrol "/begin"
-herccontrol "/tape dump * * e" -w "^Ready;"
+herccontrol "/access 193 e" -w "^Ready"
+herccontrol "/copyfile brexx * a = = e" -w "^Ready"
+herccontrol "/copyfile brexxd * a = = e" -w "^Ready"
+herccontrol "/tape dump * * e" -w "^Ready"
 herccontrol "/detach 181" -w "^Ready;"
 herccontrol "/vmarc pack * * e (pun" -w "^Ready;"
 
-# Clean Up
-herccontrol "/erase brexx * a" -w "^Ready;"
-herccontrol "/erase brexxd * a" -w "^Ready;"
-herccontrol "/erase rexxtry exec a" -w "^Ready;"
-herccontrol "/erase sysprof exec a" -w "^Ready;"
+# Close and remove extra record from VMARC file
+herccontrol "devinit 00d dummy" -w "^HHCPN098I"
+truncate -s-80 brexxbin.vmarc
 
 # LOGOFF
 herccontrol "/logoff" -w "^VM/370 Online"
 
+# REBUILD CMS
+herccontrol "/logon maint cpcms" -w "^CMS"
+herccontrol "/" -w "^Ready"
+herccontrol "/NEWBREXX" -w "^Ready"
+herccontrol "/define storage 16m"  -w "CP ENTERED"
+herccontrol "/ipl 190 clear" -w "^CMS"
+herccontrol "/savesys cms" -w "^CMS"
+herccontrol "/" -w "^Ready;"
+herccontrol "/logoff" -w "^VM/370 Online"
+
+# Test suite
+# herccontrol "/runtest_" -w "^Ready;"
+
 # SHUTDOWN
 herccontrol "/logon operator operator" -w "RECONNECTED AT"
 herccontrol "/shutdown" -w "^HHCCP011I"
+herccontrol "detach 09F0"
--- cp2vm370.sh
+++ cp2vm370.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Copy and build source files to VM370
-# This tests the cmsbuild.sh and cmsinstall.sh scripts
-# that are used by the automated build process
+# This tests the cmsbuild.sh script
+# that is used by the automated build process
 
 # STOP MSYS2 rewriting directory paths in the docker container
 export MSYS2_ARG_CONV_EXCL="vm370:;/opt"
@@ -9,37 +9,46 @@ export MSYS2_ARG_CONV_EXCL="vm370:;/opt"
 docker kill vm370
 docker pull adriansutherland/vm370:builder
 docker run --rm -d -p 3270:3270 -p 8038:8038 -p 3505:3505 --name vm370 adriansutherland/vm370:builder
-# docker run --rm -d -p 3270:3270 -p 8038:8038 -p 3505:3505 --name vm370 adriansutherland/vm370local:latest
 
 yata -c -f tmp.txt
-echo "USERID  CMSUSER\n:READ  YATA     TXT     " > yata.txt
-cat tmp.txt >> yata.txt
-docker cp yata.txt vm370:/opt/hercules/vm370/io
-rm yata.txt
+echo "USERID  CMSUSER\n:READ  ARCHIVE  YATA    " > archive.yata
+cat tmp.txt >> archive.yata
+docker cp archive.yata vm370:/opt/hercules/vm370/io
+rm archive.yata
 rm tmp.txt
 docker exec vm370 bash -c "cd /opt/hercules/vm370/io && yata -x"
-docker exec vm370 bash -c "rm /opt/hercules/vm370/io/yata.txt"
+docker exec vm370 bash -c "rm /opt/hercules/vm370/io/archive.yata"
 
 # Test Suite
 yata -c -d tests -f tmp.txt
-echo "USERID  CMSUSER\n:READ  YATA     TXT     " > yata.txt
-cat tmp.txt >> yata.txt
-docker cp yata.txt vm370:/opt/hercules/vm370/io
+echo "USERID  CMSUSER\n:READ  ARCHIVE  YATA    " > archive.yata
+cat tmp.txt >> archive.yata
 docker exec vm370 bash -c "mkdir /opt/hercules/vm370/io/tests"
-docker cp yata.txt vm370:/opt/hercules/vm370/io/tests
-rm yata.txt
+docker cp archive.yata vm370:/opt/hercules/vm370/io/tests
+rm archive.yata
 rm tmp.txt
 docker exec vm370 bash -c "cd /opt/hercules/vm370/io/tests && yata -x"
-docker exec vm370 bash -c "rm /opt/hercules/vm370/io/tests/yata.txt"
+docker exec vm370 bash -c "rm /opt/hercules/vm370/io/tests/archive.yata"
+
+# Tools
+yata -c -d tools -f tmp.txt
+echo "USERID  CMSUSER\n:READ  ARCHIVE  YATA    " > archive.yata
+cat tmp.txt >> archive.yata
+docker exec vm370 bash -c "mkdir /opt/hercules/vm370/io/tools"
+docker cp archive.yata vm370:/opt/hercules/vm370/io/tools
+rm archive.yata
+rm tmp.txt
+docker exec vm370 bash -c "cd /opt/hercules/vm370/io/tools && yata -x"
+docker exec vm370 bash -c "rm /opt/hercules/vm370/io/tools/archive.yata"
 
 # Build
-#docker cp cmsbuild.sh vm370:/opt/hercules/vm370/io
-#docker exec vm370 bash -c "cd /opt/hercules/vm370/io && chmod +x cmsbuild.sh && ./cmsbuild.sh"
-docker cp cmsdebug.sh vm370:/opt/hercules/vm370/io
-docker exec vm370 dos2unix /opt/hercules/vm370/io/cmsdebug.sh
-docker exec vm370 bash -c "cd /opt/hercules/vm370/io && chmod +x cmsdebug.sh && ./cmsdebug.sh"
+docker cp cmsbuild.sh vm370:/opt/hercules/vm370/io
+docker exec vm370 dos2unix /opt/hercules/vm370/io/cmsbuild.sh
+docker exec vm370 bash -c "cd /opt/hercules/vm370/io && chmod +x cmsbuild.sh && ./cmsbuild.sh"
+#docker cp cmsdebug.sh vm370:/opt/hercules/vm370/io
+#docker exec vm370 dos2unix /opt/hercules/vm370/io/cmsdebug.sh
+#docker exec vm370 bash -c "cd /opt/hercules/vm370/io && chmod +x cmsdebug.sh && ./cmsdebug.sh"
 
-# Install
-#docker cp cmsinstall.sh vm370:/opt/hercules/vm370/io
-#docker exec vm370 dos2unix /opt/hercules/vm370/io/cmsinstall.sh
-#docker exec vm370 bash -c "cd /opt/hercules/vm370/io && chmod +x cmsinstall.sh && ./cmsinstall.sh"
+# IPL VM
+herccontrol "attach 09F0 3350 gccbrx.cckd"
+herccontrol "ipl 141" -w "USER DSC LOGOFF AS AUTOLOG1"
